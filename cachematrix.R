1980e22722523ac599202793ea2fdc671121d75c
Pamela <ggomezpamela@gmail.com>
2018-02-25 21:47:39 -0600

    submission for wk 2 assignment
    Please review my submission to the second week assignemnt.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## The function makeCacheMatrix sets the input data to a matrix, gets the matrix, sets the value of the inverse, gets the value of the inverse. 
+
+makeCacheMatrix <- function(x = matrix()){
+	minv <- NULL
+  set <-function(y) {
+         x <<- y
+         minv <<- NULL
+      }
+      get <-function ()x
+      setinverse <- function(inverse)  minv <<- inverse
+      getinverse <- function() minv
+      list(set=set, get=get, 
+           setinverse=setinverse,
+           getinverse=getinverse)
+   
 
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of the square matrix (data) that was inputed if it hasn't been found cached data. 
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) {minv <-x$getinverse()
+  if(!is.null(minv)) {
+          message ("getting cached data")
+          return (minv)
+  }
+  data <- x$get()
+  minv <-solve(data,...)
+  x$setinverse(minv)
+  minv
+}
+aMatrix<-makeCacheMatrix(matrix(1:4,2,2))
+aMatrix$get()
+cacheSolve(aMatrix)
         ## Return a matrix that is the inverse of 'x'
 }
